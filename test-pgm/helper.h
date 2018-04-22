035e15a18f9ad61076918bc7666091249d656a98
Edward Erik <erikschmoll@gmail.com>
2018-04-22 12:28:34 -0300

    agrego el open, write y close

--- test-pgm/helper.h
+++ test-pgm/helper.h
@@ -14,4 +14,13 @@ my_print(char* value, int length);
 extern void
 convert_to_str(char* buf,unsigned c);
 
+extern int
+my_write(FILE* f, const void *buf, size_t nbyte);
+
+extern short
+my_open(const char *path, int oflag);
+
+extern void
+my_close(FILE* f);
+
 #endif
--- test-pgm/main.c
+++ test-pgm/main.c
@@ -4,18 +4,16 @@
 int
 main(int argc, char* const argv[]){
     char p[5];
+    FILE* t;
     printf("hola mundo \n");
     nop();
     caller_nop();
     my_print("a", 1);
     my_print("\n", 1);
     convert_to_str(p, (unsigned)100);
-    my_print(p, 5);
-    convert_to_str(p, (unsigned)1);
-    my_print(p, 5);
-    convert_to_str(p, (unsigned)251);
-    my_print(p, 5);
-    convert_to_str(p, (unsigned)20);
-    my_print(p, 5);
+    //my_write(stdout, p, 5);
+    t = fopen("hola.txt", "w");
+    my_write(t, p, 5);
+    my_close(t);
     return 0;
 }
